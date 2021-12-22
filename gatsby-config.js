a98fbc9b981edc815af36f868225ab06cb8e1fd3
dongjinBaek <dj9136@gmail.com>
2021-12-22 15:49:36 +0900

    gtag 설정 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
         // This config will be shared across all trackingIds
         gtagConfig: {
           // optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
+          // anonymize_ip: true,
           cookie_expires: 0,
         },
         // This object is used for configuration specific to this plugin
